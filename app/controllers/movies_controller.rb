6fef18fd77d013fe03e65626d62df6c99937f9d2
Ben Purinton <ben.purinton@gmail.com>
2023-02-01 20:15:46 +0000

    day 1 recording done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
 
   def new
+    @the_movie = Movie.new
     render template: "movies/new"
+  end
+
+  def edit
+    @the_movie = Movie.where(id: params[:id]).first
+    render template: "movies/edit"
   end
 
   def index
@@ -23,16 +29,17 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      # redirect_to("/movies/new", { :alert => @the_movie.errors.full_messages.to_sentence })
+      render template: "movies/new"
     end
   end
 
