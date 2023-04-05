73bfbd827adfca6756fb6a8b38e0355cdcaf3516
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 00:24:47 +0000

    wip

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,10 +1,28 @@
 require "rails_helper"
 
 describe "The /movies page" do
+
   it "can be visited", points: 1 do
     visit "/movies"
-    
+
     expect(page.status_code).to be(200),
       "Expected to visit /movies successfully, but didn't find it."
+  end
+
+  it "has a link to add a movie", points: 1 do
+    visit "/movies"
+
+    expect(page).to have_link('Add a new movie', href: "/movies/new"),
+      "Expected /movies to have an 'Add a new movie' link to '/movies/new'"
+  end
+end
+
+
+describe "The /books page" do
+  it "can be visited", points: 1 do
+    visit "/books"
+    
+    expect(page.status_code).to be(200),
+      "Expected to visit /books successfully, but didn't find it."
   end
 end
