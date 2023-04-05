cc6ead8a394818673ae965fc86d7cee3b9d295b4
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 02:51:09 +0000

    wip

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,40 +1,46 @@
 require "rails_helper"
 
 describe "The /movies page" do
-  it "can be visited", points: 1 do
+  before do
     visit "/movies"
+  end
 
+  it "can be visited", points: 1 do
     expect(page.status_code).to be(200),
-      "Expected to visit /movies successfully, but couldn't."
+      "Expected to visit /movies successfully."
   end
 
   it "has a link to add a movie", points: 1 do
-    visit "/movies"
-
     expect(page).to have_link('Add a new movie', href: "/movies/new"),
-      "Expected /movies to have an 'Add a new movie' link to '/movies/new', but link wasn't there or had a typo."
+      "Expected /movies to have an 'Add a new movie' link to '/movies/new'."
   end
 end
 
 describe "The /movies/new page" do
-  it "can be visited", points: 1 do
+  before do
     visit "/movies/new"
+  end
 
+  it "can be visited", points: 1 do
     expect(page.status_code).to be(200),
-      "Expected to visit /movies/new successfully, but couldn't."
+      "Expected to visit /movies/new successfully."
   end
 
   it "has a form", points: 1 do
-    visit "/movies/new"
-
-    expect(page).to have_selector("form"),
-      "Expected /movies/new to have a form, but didn't find the form element."
+    expect(page).to have_selector("form[action='/movies']"),
+      "Expected /movies/new to have a form with action='/movies'."
   end
 
   it "has a hidden authenticity token input", points: 1 do
-    visit "/movies/new"
-
     expect(page).to have_selector("input[name='authenticity_token']", visible: false),
       "Expected the new movie form to have an input field of type='hidden' and name='authenticity_token'."
+  end
+
+  it "creates a movie successfully", point: 1 do
+    fill_in "Title", with: "My test movie"
+    fill_in "Description", with: "description"
+    click_button "Create movie"
+    expect(page).to have_content("Movie created successfully."),
+      "Expected to fill in the new movie form, click 'Create movie', and be redirected to the movie index with a success notice"
   end
 end
