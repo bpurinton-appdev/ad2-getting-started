1b1cfc0ddd6e4f935d91730138c2a732d55c0da5
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 02:01:11 +0000

    wip

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -5,32 +5,42 @@ describe "The /movies page" do
     visit "/movies"
 
     expect(page.status_code).to be(200),
-      "Expected to visit /movies successfully, but didn't find it."
+      "Expected to visit /movies successfully, but couldn't."
   end
 
   it "has a link to add a movie", points: 1 do
     visit "/movies"
 
     expect(page).to have_link('Add a new movie', href: "/movies/new"),
-      "Expected /movies to have an 'Add a new movie' link to '/movies/new'"
+      "Expected /movies to have an 'Add a new movie' link to '/movies/new', but link wasn't there or had a typo."
   end
 end
 
-describe "The /movies/new" do
+describe "The /movies/new page" do
   it "can be visited", points: 1 do
     visit "/movies/new"
 
     expect(page.status_code).to be(200),
-      "Expected to visit /movies/new successfully, but didn't find it."
+      "Expected to visit /movies/new successfully, but couldn't."
   end
 
-  it "has a 'Create movie' form", points: 1 do
+  it "has a form", points: 1 do
     visit "/movies/new"
 
-    expect(page).to have_selector("forms")
+    expect(page).to have_selector("form"),
+      "Expected /movies/new to have a form, but didn't find the form element."
 
     # expect(page).to have_link('Create movie', href: "/movies"),
     #   "Expected /movies/new to have a 'Create movie' form button to '/movies/new'"
+  end
+
+  it "has an authenticity token input", points: 1 do
+    visit "/movies/new"
+
+    expect(page).to have_selector("input[name='authenticity_token']")
+    # expect(page).to have_selector('input[type="hidden"]')
+    # expect(page).to have_field('Descripton')
+    # expect(page).to have_selector("input[name='query_titl']")
   end
 end
 
