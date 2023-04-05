8846988d2108d0b1389beea0e8dfdf9d18bb651f
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 03:09:55 +0000

    wip

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -63,22 +63,4 @@ describe "The movie details page" do
     expect(page).to have_selector("a[href='/movies/#{ @movie.id }'][data-method='delete']", text: 'Delete movie'),
       "Expected /movies/ID to have 'Delete movie' link with the proper data-method='delete'."
   end
-
-  # it "has a form", points: 1 do
-  #   expect(page).to have_selector("form[action='/movies']"),
-  #     "Expected /movies/new to have a form with action='/movies'."
-  # end
-
-  # it "has a hidden authenticity token input", points: 1 do
-  #   expect(page).to have_selector("input[name='authenticity_token']", visible: false),
-  #     "Expected the new movie form to have an input field of type='hidden' and name='authenticity_token'."
-  # end
-
-  # it "creates a movie successfully", point: 1 do
-  #   fill_in "Title", with: "My test movie"
-  #   fill_in "Description", with: "description"
-  #   click_button "Create movie"
-  #   expect(page).to have_content("Movie created successfully."),
-  #     "Expected to fill in the new movie form, click 'Create movie', and be redirected to the movie index with a success notice"
-  # end
 end
